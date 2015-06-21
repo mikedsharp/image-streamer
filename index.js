9e0def37e1274616604f57eb02222a8bd3eb4c87
programmerinprogress <msharp23@hotmail.co.uk>
2015-06-21 18:06:43 +0100

    working on core API for Instagram Auth

--- index.js
+++ index.js
@@ -8,18 +8,36 @@ app.get('/', function(request, response) {
   response.send("Hello world again");
 });
 
-app.get('/bacon', function(request, response) {
+app.get('/subscription/request', function(request, response) {
 
-   if(typeof request.query["hub.challenge"] !== "string"){
+   if(typeof request.query["hub.challenge"] !== "string"){ // ignore as this isn't from instagram API
       response.send("bacon"); 	
     }
-	else{
+	else{ //if the call is successful, echo back challenge
       response.send( request.query["hub.challenge"]); 	
 	}
   
-
-
 });
+
+app.get('/authorize/success', function(request, response){
+	response.send("Authorization successful!"); 
+}); 
+
+app.get('/authorize', function(request, response){
+	var authEndpoint = ''; 
+	var client_id = '617bb2656c9d46ccbc3a603106230bf0'; 
+	var client_secret = ''; 
+	var redirect_uri = 'https://mike-s-imagestreamer.herokuapp.com/authorize/success'; 
+
+	authEndpoint = 'https://api.instagram.com/oauth/authorize/?client_id='
+	+ client_id 
+	+ '&redirect_uri=' 
+	+  redirect_uri
+	+ '&response_type=code'; 
+
+
+	response.redirect(authEndpoint); 
+}); 
 
 
 app.listen(app.get('port'), function() {
