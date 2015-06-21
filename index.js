6338668832b468fa463c25e768ec4d6fd0f775fb
programmerinprogress <msharp23@hotmail.co.uk>
2015-06-21 15:36:11 +0100

    Added new route

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2nd attempt!');
+  response.send("Hello world again");
 });
+
+app.get('/bacon', function(request, response) {
+  response.send("Hello world again");
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
