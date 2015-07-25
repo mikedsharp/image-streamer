c29b275feb1d3d5a8ac947ec1e819496716834d0
programmerinprogress <msharp23@hotmail.co.uk>
2015-07-25 20:32:26 +0100

    just testing new linter, followed its instructions and tidied up a little

--- server.js
+++ server.js
@@ -1,142 +1,142 @@
-//server.js 
+//server.js
 
-// packages 
+// packages
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var bodyParser = require('body-parser'); 
-var needle = require('needle'); 
+var bodyParser = require('body-parser');
+var needle = require('needle');
 var path = require('path');
 var os = require("os");
 var subscriptionAPI = require('./subscriptionapi.js');
 
-// globals 
-var subscribedTags = []; 
-var maxsubscriptionlife = 60000; 
-var clientId =  process.env.CLIENT_ID; 
-var clientSecret = process.env.CLIENT_SECRET;  
-var redirect = process.env.HOST_URL; 
+// globals
+var subscribedTags = [];
+var maxsubscriptionlife = 60000;
+var clientId =  process.env.CLIENT_ID;
+var clientSecret = process.env.CLIENT_SECRET;
+var redirect = process.env.HOST_URL;
 
 var environment = 'production';
 
-var subscriptionInt; 
+var subscriptionInt;
 
 var port = process.env.PORT || 5000;
 
 var io = require('socket.io').listen(app.listen(port));
 
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());  
+app.use(bodyParser.json());
 app.use("", express.static(path.join(__dirname, 'public')));
 
-// API ROUTES 
-var router = express.Router(); 
+// API ROUTES
+var router = express.Router();
 
 router.use(function(req, res, next){
 	// let routing propagate
-	next(); 
-}); 
+	next();
+});
 
 // all api requests will be routed through here
 router.get('/api', function  (request, response) {
-	response.json({message: 'api reached'}); 
-}); 
+	response.json({message: 'api reached'});
+});
 
-// TAG ROUTES 
+// TAG ROUTES
 router.route('/api/tag')
 	.post(function(request, response){
-		// obtain tagname and see if we already have it, if we do, update its expiration date, otherwise, call subscription APIs 
-		var hashtag = request.body.hashtag; 
+		// obtain tagname and see if we already have it, if we do, update its expiration date, otherwise, call subscription APIs
+		var hashtag = request.body.hashtag;
 		var isSubscribed = subscribedTags.filter(function(value){
-			return value.hashtag == hashtag; 
-		}).length > 0; 
+			return value.hashtag == hashtag;
+		}).length > 0;
 
 		if(isSubscribed){
 			//no need to add, just update the max life
 			for(currentsub in subscribedTags){
 				if(subscribedTags[currentsub].hashtag == 'hashtag'){
-					subscribedTags[currentsub].maxlife = new Date(new Date().getTime() + maxsubscriptionlife).getTime(); 
+					subscribedTags[currentsub].maxlife = new Date(new Date().getTime() + maxsubscriptionlife).getTime();
 				}
 			}
 		}
 		else{
-			//call instagram API for real time subscription 
+			//call instagram API for real time subscription
 			var post_data = {
 				"hashtag" : hashtag
 			}
-			var options = []; 
+			var options = [];
 
 			needle.post(redirect + '/api/subscription', post_data, options, function(err, resp){
-			}); 
+			});
 
 		}
-		response.json({message: "response request sent !"}); 
+		response.json({message: "response request sent !"});
 
 	})
 	.get(function(request, response){
-	response.json(subscribedTags); 
+	response.json(subscribedTags);
 });
 
 
 router.route('/api/tag/heartbeat')
 	.post(function(request, response){
-		subscriptionAPI.processHeartbeat(request.body); 
-		response.send(''); 
+		subscriptionAPI.processHeartbeat(request.body);
+		response.send('');
 
-}); 
+});
 
 router.route('/api/tag/:hashtag')
 	.get(function(request, response){
 	for(currentsub in subscribedTags){
 		if(subscribedTags[currentsub].hashtag == request.params.hashtag){
-			response.json(subscribedTags[currentsub]); 
+			response.json(subscribedTags[currentsub]);
 		}
 	}
-	response.json({message: 'no tag subscribed by that name.'}); 
+	response.json({message: 'no tag subscribed by that name.'});
 
 })
 	.delete(function(request, response){
 
 	var credentials = {
-				"clientId": clientId, 
+				"clientId": clientId,
 				"clientSecret": clientSecret,
 				"redirect": redirect
-			}; 
+			};
 
 	for(currentsub in subscribedTags){
 		if(subscribedTags[currentsub].hashtag == request.params.hashtag){
 			subscriptionAPI.removeSubscription(subscribedTags[currentsub].subscription_id, credentials)
 		}
 	}
-	response.json({message: 'no tag subscribed by that name.'}); 
+	response.json({message: 'no tag subscribed by that name.'});
 
-}); 
+});
 
 
 
 router.route('/api/image/:hashtag')
 	.get(function(request, response){
-	var url = 'https://api.instagram.com/v1/tags/' + request.params.hashtag + '/media/recent' + '?access_token=' +  request.query.access_token; 
+	var url = 'https://api.instagram.com/v1/tags/' + request.params.hashtag + '/media/recent' + '?access_token=' +  request.query.access_token;
 
-}); 
+});
 
 
-// INSTAGRAM SUBSCRIPTION ROUTES 
+// INSTAGRAM SUBSCRIPTION ROUTES
 
 // subscriptions (node to real time API communication)
 router.route('/api/subscription/new')
 	.post(function(request, response) {
 		// let users know in real time that there's a new image to stream
-		io.emit('new recent image', request.body); 
-		response.send(''); 
+		io.emit('new recent image', request.body);
+		response.send('');
 
 	}).get(function(request, response) {
 
    if(typeof request.query["hub.challenge"] !== "string"){ // ignore as this isn't from instagram API
-	   	response.send(""); 	
+	   	response.send("");
 	}
 	else{ //if the call is successful, echo back challenge
-		response.send(request.query["hub.challenge"]); 	
+		response.send(request.query["hub.challenge"]);
 	}
 
 });
@@ -144,17 +144,17 @@ router.route('/api/subscription/new')
 router.route('/api/subscription')
 	.post(function(request, response) {
 
-		if(typeof request.body.hashtag === "string"){ 
+		if(typeof request.body.hashtag === "string"){
 
 			var tag = request.body.hashtag;
 
 			var credentials = {
-				"clientId":clientId, 
+				"clientId":clientId,
 				"clientSecret": clientSecret,
 				"redirect": redirect
-			}; 
+			};
 
-			subscriptionAPI.addSubscription(tag, credentials); 
+			subscriptionAPI.addSubscription(tag, credentials);
 		}
 
 		response.send('');
@@ -162,80 +162,80 @@ router.route('/api/subscription')
 	}).get(function(request, response) {
 
    if(typeof request.query["hub.challenge"] !== "string"){ // ignore as this isn't from instagram API
-   	response.send(""); 	
+   	response.send("");
 	}
    else{ //if the call is successful, echo back challenge
-   	response.send(request.query["hub.challenge"]); 	
+   	response.send(request.query["hub.challenge"]);
    }
 
 });
 
-// remove add and view subscriptions, sends requests to instagram API 
+// remove add and view subscriptions, sends requests to instagram API
 router.route('/api/subscription/:id')
 	.delete(function(request, response) {
-		if(typeof request.params.id === "string"){ 
+		if(typeof request.params.id === "string"){
 
- 	 	 // remove subscriptions by subscription id 
- 	 	 var id = request.params.id; 
- 	 	 var options = []; 
+ 	 	 // remove subscriptions by subscription id
+ 	 	 var id = request.params.id;
+ 	 	 var options = [];
  	 	 var post_data = {
- 	 	 }; 
+ 	 	 };
 
  	 	 var credentials = {
-				"clientId":clientId, 
+				"clientId":clientId,
 				"clientSecret": clientSecret,
 				"redirect": redirect
-			}; 
+			};
 
- 	 	 subscriptionAPI.removeSubscription(id, credentials); 
+ 	 	 subscriptionAPI.removeSubscription(id, credentials);
  	 	 response.json({message : "subscription removed"});
 		}
- 	}); 
+ 	});
 
 	router.route('/api/user/authorize')
 	.get(function(request, response){
 
-		var authEndpoint = ''; 
-		var redirect_uri =  redirect + '/api/user/authorize/redirect'; 
+		var authEndpoint = '';
+		var redirect_uri =  redirect + '/api/user/authorize/redirect';
 
 		authEndpoint = 'https://api.instagram.com/oauth/authorize/?client_id='
-		+ clientId 
-		+ '&redirect_uri=' 
+		+ clientId
+		+ '&redirect_uri='
 		+  redirect_uri
-		+ '&response_type=code'; 
+		+ '&response_type=code';
 
-		response.redirect(authEndpoint); 
-	}); 
+		response.redirect(authEndpoint);
+	});
 
 	router.route('/api/user/authorize/redirect')
 	.get(function(request, response){
 
-		var authEndpoint = ''; 
-		var code = request.query['code']; 
-		var redirect_uri = redirect + '/api/user/authorize/redirect'; 
+		var authEndpoint = '';
+		var code = request.query['code'];
+		var redirect_uri = redirect + '/api/user/authorize/redirect';
 		var post_data = {
-			"client_id": clientId, 
+			"client_id": clientId,
 			"client_secret": clientSecret,
-			"grant_type": "authorization_code",  
-			"code": code, 
+			"grant_type": "authorization_code",
+			"code": code,
 			'redirect_uri': redirect_uri
-		}; 
-		var options = []; 
+		};
+		var options = [];
 
 		needle.post('https://api.instagram.com/oauth/access_token', post_data,  options, function(err, resp) {
-			var access_token = resp.body.access_token; 
+			var access_token = resp.body.access_token;
       		// hand access token to user to allow them to request images
-      		response.cookie('access_token', access_token); 
+      		response.cookie('access_token', access_token);
       		response.redirect('/streamer.html')
   		});
 
-	}); 
+	});
 
-	
-// register routes 
-app.use('/', router); 
 
-// start socket io server 
+// register routes
+app.use('/', router);
+
+// start socket io server
 io.sockets.on('connection', function (socket) {
 
 
@@ -243,10 +243,10 @@ io.sockets.on('connection', function (socket) {
 	if(io.engine.clientsCount == 1){
 
 		var credentials = {
-				"clientId":clientId, 
+				"clientId":clientId,
 				"clientSecret": clientSecret,
 				"redirect": redirect
-	    }; 
+	    };
 		subscriptionInt = setInterval(subscriptionAPI.subscriptionCheck, 60000, credentials);
 	}
 
@@ -259,13 +259,13 @@ io.sockets.on('connection', function (socket) {
 		if(io.engine.clientsCount == 0){
 
 			var credentials = {
-				"clientId": clientId, 
+				"clientId": clientId,
 				"clientSecret": clientSecret,
 				"redirect": redirect
-			}; 
+			};
 
-			subscriptionAPI.removeAllSubscriptions(credentials); 
-			clearInterval(subscriptionInt); 
+			subscriptionAPI.removeAllSubscriptions(credentials);
+			clearInterval(subscriptionInt);
 		}
 
 		console.log('user disconnected number connected: ' + io.engine.clientsCount);
@@ -273,6 +273,4 @@ io.sockets.on('connection', function (socket) {
 
 });
 
-console.log('server started on port: ' + port); 
-
-
+console.log('server started on port: ' + port);
--- subscriptionapi.js
+++ subscriptionapi.js
@@ -1,83 +1,83 @@
-var subscribedTags = {}; 
-var subscriptionInt; 
-var maxsubscriptionlife = 60000; 
+var subscribedTags = {};
+var subscriptionInt;
+var maxsubscriptionlife = 60000;
 
-var needle = require('needle'); 
+var needle = require('needle');
 var deleteKey = require('key-del');
 
 var self = {
 	subscriptionCheck: function(credentials){
-		for(currentsub in subscribedTags){
+		for(var currentsub in subscribedTags){
 			if(subscribedTags[currentsub].maxlife <= new Date().getTime()){
-				self.removeSubscription(currentsub, credentials); 
+				self.removeSubscription(currentsub, credentials);
 				console.log('hashtag deleted due to inactivity');
 			}
 		}
 
-	}, 
+	},
 	removeSubscription: function(hashtag, credentials){
-	  // remove subscriptions by subscription id 
-	  var id = subscribedTags[hashtag].subscription_id; 
-	  var options = []; 
+	  // remove subscriptions by subscription id
+	  var id = subscribedTags[hashtag].subscription_id;
+	  var options = [];
 	  var post_data = {
-	  
-	  }; 
 
-	  needle.delete('https://api.instagram.com/v1/subscriptions?&client_id=' + credentials.clientId + '&id=' + id + '&client_secret=' + credentials.clientSecret, post_data, options,  
+	  };
+
+	  needle.delete('https://api.instagram.com/v1/subscriptions?&client_id=' + credentials.clientId + '&id=' + id + '&client_secret=' + credentials.clientSecret, post_data, options,
 	  	(function(scope){
 
-	  				console.log('hashtag "' + hashtag  +'" deleted'); 
+	  				console.log('hashtag "' + hashtag  +'" deleted');
 	  				subscribedTags = deleteKey(subscribedTags, [hashtag]);
-	  		
-	  	}(hashtag))); 
-	}, 
-	removeAllSubscriptions: function(credentials){
-		var options = []; 
-		var post_data = {
-		}; 
 
-		for(currentsub in subscribedTags){
-			needle.delete('https://api.instagram.com/v1/subscriptions?&client_id=' + credentials.clientId + '&object=' + 'tag' + '&client_secret=' + credentials.clientSecret, post_data, options, function(){
-				console.log('All hashtags deleted');
-				subscribedTags = {};
-			}); 
+	  	}(hashtag)));
+	},
+	removeAllSubscriptions: function(credentials){
+		var options = [];
+		var post_data = {
+		};
+		for(var currentsub in subscribedTags){
+			needle.delete('https://api.instagram.com/v1/subscriptions?&client_id=' + credentials.clientId + '&object=' + 'tag' + '&client_secret=' + credentials.clientSecret, post_data, options, self.removeSubscription_success);
 		}
-	}, 
+	},
+	removeSubscription_success : function(){
+		console.log('All hashtags deleted');
+		subscribedTags = {};
+	} ,
 	addSubscription: function(hashtag, credentials){
 
 			var tag = hashtag;
-			var options = []; 
+			var options = [];
 			var post_data = {
-				"object": "tag", 
+				"object": "tag",
 				"object_id": tag,
-				"aspect": "media", 
+				"aspect": "media",
 				"callback_url": credentials.redirect + '/api/subscription/new'
-			}; 
+			};
 
 			needle.post('https://api.instagram.com/v1/subscriptions?client_id=' + credentials.clientId + '&client_secret=' + credentials.clientSecret + '&verify_token=' + 'streamapp', post_data, options, function(err,response){
 				if(response.statusCode == 200 ){
 					subscribedTags[response.body.data.object_id] =  {
-						hashtag: response.body.data.object_id, 
-						maxlife: new Date().getTime() + maxsubscriptionlife, 
+						hashtag: response.body.data.object_id,
+						maxlife: new Date().getTime() + maxsubscriptionlife,
 						subscription_id:response.body.data.id
-					}; 
+					};
 
 				}
 				else{
-		 	    	console.log("request failed"); 
+		 	    	console.log("request failed");
 		 	    }
-		 	}); 
-	
+		 	});
+
 
 	},
 	processHeartbeat: function(data){
-	 for(key in data.tags){
-		if(subscribedTags[data.tags[key]] != null && typeof subscribedTags[data.tags[key]] == "object"){
-			subscribedTags[data.tags[key]].maxlife = new Date().getTime() + maxsubscriptionlife; 
+	 for(var key in data.tags){
+		if(subscribedTags[data.tags[key]] !== null && typeof subscribedTags[data.tags[key]] == "object"){
+			subscribedTags[data.tags[key]].maxlife = new Date().getTime() + maxsubscriptionlife;
 		}
 	 }
 
-	} 
+	}
 };
 
-module.exports = self; 
+module.exports = self;
