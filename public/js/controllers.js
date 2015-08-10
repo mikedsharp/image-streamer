3aa8531d1439b261e386ab4dff455ebb64f4c7c4
programmerinprogress <msharp23@hotmail.co.uk>
2015-08-10 21:15:35 +0100

    fixed repeating images bug, setting stage for moving streamer and authentication onto one view, still working it out

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,6 +6,8 @@ controller('ImageStreamerCtrl', ['$scope', '$http', '$interval', 'HashtagService
 
   $scope.hashtags = HashtagService.hashtags;
 
+  $scope.token = null;
+
   $interval(function() {
     // let server know we're still interested in the hastags we have
     var activeTags = [];
@@ -65,49 +67,57 @@ controller('ImageStreamerCtrl', ['$scope', '$http', '$interval', 'HashtagService
     var deletedTag = $scope.hashtags.splice($scope.hashtags.indexOf(tag), 1);
   };
 
+  $scope.isAuthenticated = function() {
+    var cookies = document.cookie.split(';');
+    $scope.token = null;
+
+    for (var j = 0; j < cookies.length; j++) {
+      if (cookies[j].indexOf('access_token=') > -1) {
+        $scope.token = cookies[j].replace('access_token=', '');
+      }
+      break;
+    }
+
+    return $scope.token !== null;
+  };
+
   $scope.updateStream = function(tag) {
     for (var i = 0; i < $scope.hashtags.length; i++) {
       if ($scope.hashtags[i].name == tag) {
 
-        var cookies = document.cookie.split(';');
-        var token = '';
+        if ($scope.isAuthenticated()) {
+          // make JSONP call to get around CORS and get latest stream of images for a given tag
+          $http.jsonp('https://api.instagram.com/v1/tags/' + tag + '/media/recent?access_token=' + $scope.token + '&callback=JSON_CALLBACK&count=1&min_tag_id=' + $scope.hashtags[i].lastid)
+            .success(function(data) {
+              // obtain tag name
+              var currentTag = data.pagination.next_url.match(/(tags\/){1}([A-z0-9])+(\/media\/){1}/gi)[0].replace('tags/', '').replace('/media/', '');
 
-        for (var j = 0; j < cookies.length; j++) {
-          if (cookies[j].indexOf('access_token=') > -1) {
-            token = cookies[j].replace('access_token=', '');
-          }
-          break;
+              for (var j = 0; j < $scope.hashtags.length; j++) {
+                if ($scope.hashtags[j].name == currentTag) {
+                  for (var i = 0; i < data.data.length; i++) {
+
+                    // if we've already had this id, wait until next, we also need to make sure we don't have an older id or we risk repeats in the stream
+                    if (typeof $scope.hashtags[j].lastid == 'undefined' || $scope.hashtags[j].lastid < data.data[i].id) {
+                      // limit our carousel, taking older photos off first (first-in last-out )
+                      if ($scope.hashtags[j].images.length > 3) {
+                        $scope.hashtags[j].images.pop();
+                      }
+                      $scope.hashtags[j].images.unshift({
+                        'src': data.data[i].images.low_resolution.url
+                      });
+                      // make note of timestamp for sending to api
+                      $scope.hashtags[j].lastid = data.data[i].id;
+                    }
+
+                  }
+                  break;
+                }
+              }
+
+            });
+
         }
 
-        // make JSONP call to get around CORS and get latest stream of images for a given tag
-        $http.jsonp('https://api.instagram.com/v1/tags/' + tag + '/media/recent?access_token=' + token + '&callback=JSON_CALLBACK&count=1&min_tag_id=' + $scope.hashtags[j].lastid)
-          .success(function(data) {
-            // obtain tag name
-            var currentTag = data.pagination.next_url.match(/(tags\/){1}([A-z0-9])+(\/media\/){1}/gi)[0].replace('tags/', '').replace('/media/', '');
-
-            for (var j = 0; j < $scope.hashtags.length; j++) {
-              if ($scope.hashtags[j].name == currentTag) {
-                for (var i = 0; i < data.data.length; i++) {
-
-                  // if we've already had this id, wait until next
-                  if ($scope.hashtags[j].lastid != data.data[i].id) {
-                    // limit our carousel, taking older photos off first (first-in last-out )
-                    if ($scope.hashtags[j].images.length > 3) {
-                      $scope.hashtags[j].images.pop();
-                    }
-                    $scope.hashtags[j].images.unshift({
-                      'src': data.data[i].images.low_resolution.url
-                    });
-                    // make note of timestamp for sending to api
-                    $scope.hashtags[j].lastid = data.data[i].id;
-                  }
-
-                }
-                break;
-              }
-            }
-
-          });
 
         break;
       }
